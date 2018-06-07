e4f70df6f5512da6dcff8bddff16f107c0fc0ca5
Kira-Kotova <35397066+Kira-Kotova@users.noreply.github.com>
2018-06-07 12:27:29 +0300

    Update StackedHistogram.cs

--- Diagrams/StackedHistogram.cs
+++ Diagrams/StackedHistogram.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
@@ -9,8 +8,15 @@ namespace Diagrams
 {
     public class StackedHistogram : AbstractDiagram
     {
+        /// <summary>
+        /// Хранилище данных для данной диаграммы.
+        /// </summary>
         private DataProvider Provider;
         
+        /// <summary>
+        /// Конструктор Нормированной гистограммы с накоплением.
+        /// </summary>
+        /// <param name="data">Данные для прорисовки.</param>
         public StackedHistogram(DataProvider data) : base(data)
         {
             Provider = new DataProvider(data.Series);
@@ -18,36 +24,75 @@ namespace Diagrams
 
         public override void Draw(Canvas canvas)
         {
-            var validHeight = canvas.ActualHeight;
-            var delta = (canvas.ActualWidth / Provider.Series.Count);
-            var dx = delta - 10; // *1
-            var x = 0.0;
+            var delta = canvas.ActualWidth / Provider.Series.Count - 50;
+            const int h = 10; // для грамотного отображения графика(при малых значениях)
+            var dx = delta - h;
+            var x = 0.0;;
 
-            for (var j =0; j < Provider.Series.Count; j++)
+            for (var j = 0; j < Provider.Series.Count; j++)
             {
                 var array = Provider.Series[j];
-                var y = 0.0;
+                var y = 15.0;
                 var dy = 0.0;  
-                var sum = array.Sum();
-
+                
+                // Warning! Холст перевёрнут! Трансформация необходима для грамотного отображения содержимого.
+                var transform = new TransformGroup();
+                transform.Children.Add(new ScaleTransform(-1, 1));
+                transform.Children.Add(new RotateTransform(180));
+                
                 for(var i = 0; i < array.Length; i++)
                 {
-                    var max = array.Sum();
-                    if (max > validHeight)
-                        max = validHeight;
-                    dy += (array[i] * max/ sum) * 10; // *1
+                    dy += array[i] * h + 15.0;
                     var a = new Point(x, y);
                     var b = new Point(dx, y);
                     var c = new Point(dx, dy);
                     var d = new Point(x, dy);
+
                     var figure = new Polygon()
                     {
                         Fill = new SolidColorBrush(Provider.ColorOfDiagram[i]),
                         Points = new PointCollection(new List<Point>(){a, b, c, d})
                     };
+                                       
+                    var axisX = new TextBlock
+                    {
+                        Text = j.ToString(),
+                        LayoutTransform = transform,
+                        Margin = new Thickness((delta - h)/2 + (delta * j),0,0,0)
+                    };
+                    
+                    var marker = new TextBlock
+                    {
+                        Text = array[i].ToString(),
+                        TextAlignment = TextAlignment.Center,
+                        LayoutTransform = transform,
+                        Margin = new Thickness((delta - h)/2 + (delta * j), y + (dy - y - 15)/2, 0, 0) 
+                    };
+                    
+                    //Legend
+                    var series = new TextBlock
+                    {
+                        Text = (i+1).ToString(),
+                        LayoutTransform = transform,
+                        Margin = new Thickness(canvas.ActualWidth - 30,100 + i * 20,0,0)
+                    };
+                    
+                    //Legend
+                    var pic = new TextBlock
+                    {
+                        Background = new SolidColorBrush(Provider.ColorOfDiagram[i]),
+                        Height = 12,
+                        Width = 12,
+                        Margin = new Thickness(canvas.ActualWidth - 50,100 + i * 20,0,0)
+                    };
+                    
                     y = dy;
-
+                    
                     canvas.Children.Add(figure);
+                    canvas.Children.Add(axisX);
+                    canvas.Children.Add(marker);
+                    canvas.Children.Add(series);
+                    canvas.Children.Add(pic);
                 }
                 x += delta;
                 dx += delta;
@@ -55,5 +100,3 @@ namespace Diagrams
         }
     }
 }
-
-// *1) 10-ки для грамотного отображения графика на холсте
