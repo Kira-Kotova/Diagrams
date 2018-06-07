81e1f42fa565138724830d76e25430f45d50c7ea
Kira-Kotova <35397066+Kira-Kotova@users.noreply.github.com>
2018-06-07 12:25:13 +0300

    Update AbstractDiagram.cs

--- Diagrams/AbstractDiagram.cs
+++ Diagrams/AbstractDiagram.cs
@@ -1,21 +1,32 @@
-﻿using System.Windows.Controls;
+using System.Windows.Controls;
 
 namespace Diagrams
 {
+    /// <summary>
+    /// Тип поддерживаемых диаграмм
+    /// </summary>
     public enum DiagramType
     {
         PieChart,
         NormalizedHistogram,
         StackedHistogram,
-        Plot
+        Plot,
+        Scatter
     }
 
     public abstract class AbstractDiagram : Canvas
     {
         private DataProvider DataInfo;
-        
+        /// <summary>
+        /// Метод прорисовки диаграммы
+        /// </summary>
+        /// <param name="canvas">Холст для прорисовки</param>
         public abstract void Draw(Canvas canvas);
 
+        /// <summary>
+        /// Конструктор диаграммы
+        /// </summary>
+        /// <param name="data">Серии</param>
         public AbstractDiagram(DataProvider data)
         {
             DataInfo = data;
